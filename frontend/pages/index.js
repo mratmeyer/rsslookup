76a6025ad8cac69e592516cff65ddb06e1f4b024
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2022-05-30 04:20:09 +0000

    Add copy button

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { useEffect, useState, useRef } from 'react'
 
 import { NextSeo } from 'next-seo';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 import HCaptcha from '@hcaptcha/react-hcaptcha';
 
@@ -75,12 +76,16 @@ export default function Home() {
                 { response.status == 200
                   ? <div>
                     {response.result.map((feed) => (
-                    <div key={feed} className="flex bg-white p-4 mb-2 rounded-md shadow-sm">
-                      <span className="text-slate-700">{feed}</span>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-slate-500 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
-                      </svg>
-                    </div>
+                      <div key={feed} className="flex bg-white p-4 mb-2 rounded-md shadow-sm">
+                        <span className="text-slate-700">{feed}</span>
+                        <CopyToClipboard text={feed} onCopy={() => alert("Copied!")}>
+                          <button className="ml-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+                            </svg>
+                          </button>
+                        </CopyToClipboard>
+                      </div>
                     ))}
                   </div>
                   : <div className="bg-white rounded-md bg-red-300 p-3 px-4">
