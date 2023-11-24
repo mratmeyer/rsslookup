391b6f97d69a1121734bbe030797aa0c53536f38
mratmeyer <17440617+mratmeyer@users.noreply.github.com>
2023-11-24 14:56:31 -0500

    Switch back Shynet

--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
             <link href="https://fonts.bunny.net/css?family=inter:400,600,700" rel="stylesheet" />
-
-            <script defer data-domain="rsslookup.com" src="https://plausible.io/js/script.js"></script>
         </Head>
         <div className="bg-gray-100">
           <div className="m-auto max-w-2xl p-6 lg:p-12 min-h-screen">
@@ -27,6 +25,11 @@ class MyDocument extends Document {
                 <p className="font-bold">© Copyright <a className="underline hover:opacity-75" target="_blank" rel="noreferrer noopener" href="https://www.maxratmeyer.com/?utm_source=rsslookup">Max Ratmeyer</a> 2023</p>
                 <p className="font-bold">View source on <a className="underline hover:opacity-75" target="_blank" rel="noreferrer noopener" href="https://github.com/mratmeyer/rsslookup">GitHub</a></p>
               </div>
+
+              <noscript>
+                <img src="https://shynet.maxnet.work/ingress/1847cdec-a434-4f36-8828-af41904ffa35/pixel.gif"></img>
+              </noscript>
+              <script defer src="https://shynet.maxnet.work/ingress/1847cdec-a434-4f36-8828-af41904ffa35/script.js"></script>
             </body>
           </div>
         </div>
