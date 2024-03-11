15c41bd7217c34b3717f79378dff174dfa3d4a61
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2024-03-11 16:48:37 -0400

    Update intro copy

--- components/Intro.js
+++ components/Intro.js
@@ -1,7 +1,7 @@
 export function Intro(props) {
     return <div>
         <h1 className="text-6xl font-semibold mb-8 leading-tight"><img src="/magnifying-glass.png" alt="Magnifying Glass Emoji" className="inline mr-3" width="50" height="50"></img>RSS Lookup</h1>
-        <h2 className="text-3xl font-semibold mb-8 leading-tight">A free tool to find the RSS feed for any website</h2>
+        <h2 className="text-3xl font-semibold mb-8 leading-tight">A free, open-source tool to find RSS feeds on any URL</h2>
         <p className="text-xl mb-8 leading-normal">Can't find the RSS feed for a website or blog? Paste the URL below and we'll try and find it!</p>
     </div>
 }
