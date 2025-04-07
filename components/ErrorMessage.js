b6d93a97c79110211883452c57b54834e4fa4bde
mratmeyer <17440617+mratmeyer@users.noreply.github.com>
2025-04-07 15:57:48 -0400

    Update error message css

--- components/ErrorMessage.js
+++ components/ErrorMessage.js
@@ -1,6 +1,6 @@
 export function ErrorMessage(props) {
   return (
-    <div className="bg-white rounded-md bg-red-300 p-3 px-4">
+    <div className="rounded-md bg-red-300 mt-8 p-3 px-4">
       <span>
         <b>Error:</b> {props.message}
       </span>
