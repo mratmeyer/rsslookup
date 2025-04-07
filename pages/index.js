ac8a1262ef28a5693a54f332750738c89e10ae27
mratmeyer <17440617+mratmeyer@users.noreply.github.com>
2025-04-07 15:52:56 -0400

    Fix react className

--- pages/index.js
+++ pages/index.js
@@ -107,8 +107,8 @@ export default function Home() {
             {loading ? (
                 <div className="flex justify-center items-center mt-12">
                   <svg xmlns="http://www.w3.org/2000/svg" className="animate-spin h-8 w-8" fill="none" viewBox="0 0 24 24">
-                    <circle class="opacity-100" cx="12" cy="12" r="10" stroke="white" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    <circle className="opacity-100" cx="12" cy="12" r="10" stroke="white" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                   </svg>
                 </div>
               ) : (
