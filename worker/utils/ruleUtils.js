3c5dc52e93a415da7a649c5152164c678f8146b8
mratmeyer <17440617+mratmeyer@users.noreply.github.com>
2025-04-25 13:54:37 -0400

    Fix ruleUtils variable :/

--- worker/utils/ruleUtils.js
+++ worker/utils/ruleUtils.js
@@ -6,7 +6,7 @@
  */
 export function parseURLforRules(fullURL, hostname, feedsSet) {
   if (hostname == 'www.reddit.com') {
-    let filter = urlString.replace(/#.*$/, ''); // Remove fragment identifiers
+    let filter = fullURL.replace(/#.*$/, ''); // Remove fragment identifiers
     filter = filter.replace(/\?.*$/, ''); // Remove query parameters
     filter = filter.replace(/\/$/, ''); // Remove trailing slash
     feedsSet.add(filter + ".rss")
