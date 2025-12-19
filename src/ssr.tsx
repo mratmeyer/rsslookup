b00c287a3ee691ccf16b1d443b9433a75218bedc
mratmeyer <17440617+mratmeyer@users.noreply.github.com>
2025-12-19 05:47:00 +0000

    cleanup

--- src/ssr.tsx
+++ src/ssr.tsx
@@ -2,16 +2,5 @@ import {
   createStartHandler,
   defaultStreamHandler,
 } from "@tanstack/react-start/server";
-import { handleURLShortcut } from "./lib/urlUtils";
 
-const handler = createStartHandler(defaultStreamHandler);
-
-export default (request: Request, ...args: any[]) => {
-  const url = new URL(request.url);
-  const shortcut = handleURLShortcut(url.pathname);
-  if (shortcut) return shortcut;
-
-  return (handler as any).fetch
-    ? (handler as any).fetch(request, ...args)
-    : (handler as any)(request, ...args);
-};
+export default createStartHandler(defaultStreamHandler);
