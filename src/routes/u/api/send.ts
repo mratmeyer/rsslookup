b6c2761317c90df9aeb1126271a895c94aae4c07
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2025-12-20 17:25:05 +0000

    fix route conflicts

--- src/routes/u/api/send.ts
+++ src/routes/u/api/send.ts
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/u/send")({
+export const Route = createFileRoute("/u/api/send")({
   server: {
     handlers: {
       POST: async ({ request }) => {
