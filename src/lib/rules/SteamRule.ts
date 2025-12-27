876d296afa6431e1186299c0b4359f729379e179
Reed <n/a>
2025-12-27 16:43:07 -0600

    Add Steam Rule to get feed url from store page or news page

---
+++ src/lib/rules/SteamRule.ts
@@ -0,0 +1,44 @@
+import type { FeedsMap } from "../types";
+import type { SiteRule, RuleContext } from "./SiteRule";
+
+/**
+ * Rule for discovering RSS feeds on YouTube.
+ * Handles channels, users, playlists, and derives additional playlist feeds.
+ */
+export class SteamRule implements SiteRule {
+    readonly name = "Steam";
+
+    private readonly validHostnames = ["store.steampowered.com"];
+
+    matchesHostname(hostname: string): boolean {
+        return this.validHostnames.includes(hostname);
+    }
+
+    extractFeeds(context: RuleContext, feedsMap: FeedsMap): void {
+        const { fullURL, pathname, searchParams } = context;
+
+        this.extractUpdatesFeed(pathname, feedsMap);
+    }
+
+    /**
+     * Extract feed from /channel/UC... URLs
+     */
+    private extractUpdatesFeed(pathname: string, feedsMap: FeedsMap): void {
+        // Matches /app/{app_id}/ and /news/app/{app_id}
+        const appIdMatch = pathname.match(/^\/(news\/)?app\/(\d+)(\/|$)/)
+        if (appIdMatch) {
+            const appId = appIdMatch[2];
+            const gameNameMatch = pathname.match(/^\/(news\/)?app\/(\d+)\/([a-zA-Z0-9\_\-]+)(\/|$)/);
+            let feedName = "Game Updates";
+            if (gameNameMatch){ 
+                let gameName = gameNameMatch[3];
+                let gameNameClean = gameName.replaceAll('_', ' ');
+                feedName = gameNameClean+" Updates";
+            }
+            feedsMap.set(
+                `https://store.steampowered.com/feeds/news/app/${appId}/`,
+                {title: feedName, isFromRule: true}
+            )
+        }
+    }
+}
